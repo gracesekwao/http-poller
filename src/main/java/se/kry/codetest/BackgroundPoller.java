420f8656b115d86eba49b39b6ed53f8dd65e2bcb
Grace Sekwao <grace@nentgroup.com>
2020-05-02 09:38:42 +0200

    Implement a poller
    add a background poller for each service to see if it returns ok or fail

--- src/main/java/se/kry/codetest/BackgroundPoller.java
+++ src/main/java/se/kry/codetest/BackgroundPoller.java
@@ -1,15 +1,39 @@
 package se.kry.codetest;
 
 import io.vertx.core.Future;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.core.json.JsonObject;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class BackgroundPoller {
+  private final WebClient client;
 
-  public Future<List<String>> pollServices(Map<String, String> services) {
-    //TODO
-    return Future.failedFuture("TODO");
+  public BackgroundPoller(io.vertx.core.Vertx vertx) {
+    client = WebClient.create(vertx);
+  }
+  public List<Future<JsonObject>> pollServices(List<JsonObject> services) {
+    return services.parallelStream().map(this::poll).collect(Collectors.toList());
+  }
+
+  private Future<JsonObject> poll(JsonObject service) {
+    String url = service.getString("url");
+    Future<JsonObject> status = Future.future();
+    try {
+      client.getAbs(url)
+              .send(response -> {
+                if (response.succeeded()) {
+                  status.complete(service.put("status", 200 == response.result().statusCode() ? "OK" : "FAIL"));
+                } else {
+                  status.complete(service.put("status", "FAIL"));
+                }
+              });
+    } catch (Exception e) {
+      status.complete(service.put("status", "FAIL"));
+    }
+    return status;
   }
 }
